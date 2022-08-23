3e5d465b03f8e7b068d6061bcde3b3716045e36c
mateo <mateo.b1496@gmail.com>
2022-08-23 20:45:20 -0300

    cambios

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import './App.css';
 import "./Estilos.css";
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
+import Layout from './components/Layout';
+import Footer from './components/Footer';
+import ItemCount from './components/ItemCount';
 
 function App() {
   return (
     <div>
      <NavBar /> 
      <ItemListContainer saludo="Bienvenidos a Cerveceria Rey" />
+     <ItemCount />
+     <Layout>
+      <Footer />
+     </Layout>
     </div>
   );
 }
