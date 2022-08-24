358a3f301535b4245aafb31c4e576e1369e800b5
mateo <mateo.b1496@gmail.com>
2022-08-24 14:46:42 -0300

    cambios

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,33 +1,35 @@
 import React, { useState } from 'react';
 
-function ItemCount() {
+const ItemCount = (props) => {
   const [count, setCount] = useState(1);
-
-  let max = 10;
-
-  const initial = () =>{
-    count < max
-    ? setCount(count + 1)
-    : alert("Usted no pued seguir comprando este producto");
+   
+  const sumar = () =>{
+    if(count < props.stock){
+      setCount(count + 1)
+    } else {
+      alert("No puede seguir comprando este producto");
+    }
   }
 
-  const stock = () =>{
-    count >= 2
-    ? setCount(count - 1)
-    : alert("No hay mas stock");
+  const restar = () =>{
+    if(count > props.initial){
+      setCount(count - 1)
+    } else {
+      alert("No hay mas stock");
+    }
   }
 
-  const onAdd = () =>{
-      alert("Felicitaciones por su compra");
+  const comprar = () =>{
+      alert(props.onAdd);
   }
 
 
   return (
     <div className='botonAll'>
-        <button className='botonInitial' onClick={initial}>+</button>
-        <button className='botonStock' onClick={stock}>-</button>
+        <button className='botonInitial' onClick={sumar}>+</button>
+        <button className='botonStock' onClick={restar}>-</button>
         <h4 className='count'>Count: {count}</h4>
-        <button className='botonOnAdd' onClick={onAdd}>Comprar Producto</button>
+        <button className='botonOnAdd' onClick={comprar}>Comprar Producto</button>
     </div>
   )
 }
