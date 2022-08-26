736f151cf64a0a2ebdfc6252387c822715994200
mateo <mateo.b1496@gmail.com>
2022-08-26 17:25:10 -0300

    cambios

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function Item({item}) {
   return(
-    <div key={item.id}>
-      <div className='allCard'>
-        <h4 className='title'>{item.title}</h4>
-        <h5 className='price'>$ {item.price}</h5>
-        <h6 className='stock'>stock: {item.stock}</h6>
-        <img src={item.img}  className="img" alt="img"></img> 
+    <div  key={item.id}>
+      <div className='col-lg-4'>
+        <div className='allCard'>
+          <h4 className='title text-center'>{item.title}</h4>
+          <h5 className='price text-center'>$ {item.price}</h5>
+          <h6 className='stock text-center'>stock: {item.stock}</h6>
+          <img src={item.img}  className="img" alt="img"></img> 
+        </div>
       </div>
     </div>
   );
