76a66f72a1446e6e77bee572f63e1fa3924d132d
mateo <mateo.b1496@gmail.com>
2022-08-26 16:14:34 -0300

    nuevo componente

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { products } from "../mock/products";
+import ItemList from "../components/ItemList";
 
 const ItemListContainer = (props) => {
+  const [items, setItems] = useState([]);
+
+  useEffect(()=>{
+    const obtProducts = new Promise((res, rej)=>{
+      setTimeout(() =>{
+        res(products)
+      },2000);
+    });
+
+    obtProducts
+    .then((correct)=>{
+      setItems(correct);
+    })
+    .catch((error)=>{
+      //setItems(error)
+    });
+  }, []);
+
+  console.log(items)
+
+
   return (
     <div>
         <h1>{props.saludo}</h1>
+        <ItemList items={items} />
     </div>
   )
 }
