9ef65a929bd54cecb561d2f5f510329c891f50af
mateo <mateo.b1496@gmail.com>
2022-08-29 18:11:16 -0300

    cambios

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import ItemListContainer from './components/ItemListContainer';
 import Layout from './components/Layout';
 import Footer from './components/Footer';
 import ItemCount from './components/ItemCount';
+import EjercicioApi from './components/EjercicioApi';
 
 function App() {
  
   return (
     <div>
      <NavBar /> 
-     <ItemListContainer saludo="Bienvenidos a Cerveceria Rey" />
+     <ItemListContainer saludo="Bienvenidos a Cerveceria Rey"/>
      <ItemCount stock={10} initial={1} onAdd="Felicitaciones por su compra" />
+     {/* <EjercicioApi /> */}
      <Layout>
       <Footer />
      </Layout>
