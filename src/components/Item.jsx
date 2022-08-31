e65016bf6fee6027136350ded7fa6f0e1b058d96
mateo <mateo.b1496@gmail.com>
2022-08-31 18:36:47 -0300

    cambios

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -3,8 +3,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function Item({item}) {
   return(
-    <div className='allCard col-lg-4' key={item.id}>
-      <div>
+    <div key={item.id}>
+      <div className='allCard'>
         <h4 className='title text-center'>{item.title}</h4>
         <h5 className='price text-center'>$ {item.price}</h5>
         <h6 className='stock text-center'>stock: {item.stock}</h6>
