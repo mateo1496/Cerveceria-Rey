653c6bf11115dd0b0a450bd21aecaf990d50a6f1
mateo <mateo.b1496@gmail.com>
2022-08-31 18:38:34 -0300

    item detail

---
+++ src/components/ItemDetail.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function ItemDetail({item}) {
+  return (
+    <div style={{display: "flex"}} key={item.id}>
+      <div className='allCard'>
+        <h4 className='title text-center'>{item.title}</h4>
+        <h5 className='price text-center'>$ {item.price}</h5>
+        <h6 className='stock text-center'>stock: {item.stock}</h6>
+        <img src={item.img} className="img" alt="img"></img>
+      </div> 
+  </div>
+  );
+}
+
+export default ItemDetail
