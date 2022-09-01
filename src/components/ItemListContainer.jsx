e4f7dd3701846ada7928649e55390157ccc68e7a
mateo <mateo.b1496@gmail.com>
2022-09-01 13:23:58 -0300

    cambios

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,27 +1,46 @@
 import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import { products } from "../mock/products";
 import ItemList from "../components/ItemList";
 
 const ItemListContainer = (props) => {
   const [items, setItems] = useState([]);
+  const { id } = useParams(); //USEPARAMS ARROJA EL VALOR DEL PARAMETRO DINAMICO, EN ESTE CASO QUE VIENE DE ITEMLISTCONTAINER.
 
   useEffect(()=>{
-    const obtProducts = new Promise((res, rej)=>{
-      setTimeout(() =>{
-        res(products)
-      },2000);
-    });
+    if(id){
+      const obtProducts = new Promise((res, rej)=>{
+        const prodFilter = products.filter((prod) => prod.category === id)
+        setTimeout(() =>{
+          res(prodFilter)
+        },2000);
+      });
+      
+      obtProducts
+      .then((correct)=>{
+        setItems(correct);
+      })
+      .catch((error)=>{
+        //setItems(error)
+      });
+    } else {
+      const obtProducts = new Promise((res, rej)=>{
+        setTimeout(() =>{
+          res(products)
+        },2000);
+      });
+      
+      obtProducts
+      .then((correct)=>{
+        setItems(correct);
+      })
+      .catch((error)=>{
+        //setItems(error)
+      });
+    }
+  }, [id]); //CON LA VARIABLE ID EN EL ARRAY DE DEPENDENCIA, CADA CATEGORIA MUESTRA SUS PRODUCTOS.
 
-    obtProducts
-    .then((correct)=>{
-      setItems(correct);
-    })
-    .catch((error)=>{
-      //setItems(error)
-    });
-  }, []);
-
-  console.log(items)
+  // console.log(items)
 
 
   return (
@@ -33,3 +52,4 @@ const ItemListContainer = (props) => {
 }
 
 export default ItemListContainer;
+
