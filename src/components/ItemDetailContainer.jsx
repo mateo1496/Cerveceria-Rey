1a40b7e349805ef8dc5c9bc74c3b3b006ce453c7
mateo <mateo.b1496@gmail.com>
2022-09-01 13:23:37 -0300

    cambios

--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -1,23 +1,40 @@
 import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
 import  { products } from "../mock/products";
 import ItemDetail from './ItemDetail';
 
 function ItemDetailContainer() {
   const [item, setItems] = useState({});
+  const { id } = useParams();
+  const idProd = Number(id);
 
   useEffect(() =>{
-    const obtProduct = new Promise ((res, rej)=>{
-      const unicProduct = products.find((product) => product.id === 1)
-      setTimeout(() => {
-        res(unicProduct)
-      }, 2000);
-    })
-
-    obtProduct
-    .then((dato) =>{
-      setItems(dato);      
-    })
-  },[])
+    if(idProd){
+      const obtProduct = new Promise ((res, rej)=>{
+        const unicProduct = products.find((product) => product.id === idProd)
+        setTimeout(() => {
+          res(unicProduct)
+        }, 2000);
+      })
+      
+      obtProduct
+      .then((dato) =>{
+        setItems(dato);      
+      })
+    } else{
+      const obtProduct = new Promise ((res, rej)=>{
+        const unicProduct = products.find((product) => product.id === 1)
+        setTimeout(() => {
+          res(unicProduct)
+        }, 2000);
+      })
+      
+      obtProduct
+      .then((dato) =>{
+        setItems(dato);      
+      })
+    }
+  },[idProd])
 
   console.log(item);
 
@@ -28,3 +45,4 @@ function ItemDetailContainer() {
 }
 
 export default ItemDetailContainer
+
