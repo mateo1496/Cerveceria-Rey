1a8e77f086a99e79cf6f6813909f31b6257000e2
mateo <mateo.b1496@gmail.com>
2022-09-01 13:23:11 -0300

    cambios

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -8,7 +8,9 @@ function ItemDetail({item}) {
         <h4 className='title text-center'>{item.title}</h4>
         <h5 className='price text-center'>$ {item.price}</h5>
         <h6 className='stock text-center'>stock: {item.stock}</h6>
+        <h6 className='description text-center'>{item.description}</h6>
         <img src={item.img} className="img" alt="img"></img>
+        <button className='button'>Ver Producto</button>
       </div> 
   </div>
   );
