b5dccca4dc9b818526c5554df56c7762f5b936f4
mateo <mateo.b1496@gmail.com>
2022-09-01 13:22:53 -0300

    cambios

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from "react-router-dom"
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function Item({item}) {
@@ -8,7 +9,9 @@ function Item({item}) {
         <h4 className='title text-center'>{item.title}</h4>
         <h5 className='price text-center'>$ {item.price}</h5>
         <h6 className='stock text-center'>stock: {item.stock}</h6>
+        <h6 className='description text-center'>{item.description}</h6>
         <img src={item.img} className="img" alt="img"></img>
+        <Link to={`/item/${item.id}`}><button className='button'>Ver Producto</button></Link>
       </div> 
     </div>
   );
