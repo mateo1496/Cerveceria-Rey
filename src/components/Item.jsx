428eb0d1b118bad17ea281d5742a46359a6c2ae5
mateo <mateo.b1496@gmail.com>
2022-09-03 20:21:14 -0300

    cambios

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -7,8 +7,8 @@ function Item({item}) {
     <div key={item.id}>
       <div className='allCard'>
         <h4 className='title text-center'>{item.title}</h4>
-        <h5 className='price text-center'>$ {item.price}</h5>
-        <h6 className='stock text-center'>stock: {item.stock}</h6>
+        <h5 className='price text-center'>$ <span class="badge bg-dark">{item.price}</span></h5>
+        <h6 className='stock text-center'>Stock: <span class="badge bg-dark">{item.stock}</span></h6>
         <h6 className='description text-center'>{item.description}</h6>
         <img src={item.img} className="img" alt="img"></img>
         <Link to={`/item/${item.id}`}><button className='button'>Ver Producto</button></Link>
