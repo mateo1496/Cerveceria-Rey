d89300a0ab65f9b3e37c6ab1384cfd4004af9726
mateo <mateo.b1496@gmail.com>
2022-09-12 17:30:58 -0300

    cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { useContext } from 'react'
+import { CartContext } from './CartContext'
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function Cart() {
+
+const Cart = () => {
+  const { cart, clear, removeItem} = useContext(CartContext);
+
+  console.log(cart);
   return (
-    <div>Cart</div>
-  )
+    <>
+      <div>
+        {cart.map((prod)=>(
+          <div key={prod.id} className='cartCard' >
+           <h4 className='title text-center'>{prod.title}</h4>
+           <h5 className='price text-center'>$ <span class="badge bg-dark">{prod.price}</span></h5>
+           <h6 className='description text-center'>{prod.description}</h6>
+           <img src={prod.img} className="img" alt="img"></img>
+           <button className='button' onClick={() =>removeItem(prod.id)}>Eliminar Producto</button>
+          </div> 
+        ))}
+      </div>
+      <button onClick={clear} className="buttonDelete">Eliminar Productos</button>
+    </>
+  );
 }
 
-export default Cart
+export default Cart;
