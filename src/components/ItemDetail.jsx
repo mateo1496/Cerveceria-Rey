9d90e3aac62ab88c47e5c44eb270b4e8ff0e3467
mateo <mateo.b1496@gmail.com>
2022-09-12 17:31:20 -0300

    cambios

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,21 +1,27 @@
-import React from 'react'
 import { Link } from "react-router-dom"
 import ItemCount from "../components/ItemCount";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useState } from 'react';
+import { useContext } from 'react';
+import { CartContext } from './CartContext';
 
 function ItemDetail({item}) {
   const [cantidad, setCantidad] = useState(0);
+  const { cart, addItem } = useContext(CartContext);
+
   const onAdd = (q) => {
     setCantidad(q);
+    addItem(item, q); //ACA PASO EL PRODUCTO Y Q ES LA CANTIDAD, DECIDO QUE QUIERO MOSTRAR.
   } 
+
+  console.log(cart); //ME IMPRIME EL ESTADO
 
   return (
     <div style={{display: "flex"}} key={item.id}>
       <div className='allCard'>
         <h4 className='title text-center'>{item.title}</h4>
-        <h5 className='price text-center'>$ {item.price}</h5>
-        <h6 className='stock text-center'>stock: {item.stock}</h6>
+        <h5 className='price text-center'>$ <span class="badge bg-dark">{item.price}</span></h5>
+        <h6 className='stock text-center'>Stock: <span class="badge bg-dark">{item.stock}</span></h6>
         <h6 className='description text-center'>{item.description}</h6>
         <img src={item.img} className="img" alt="img"></img>
         <button className='button'>Ver Producto</button>
