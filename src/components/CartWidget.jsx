fb474b023f1419f935f60bbe206d4d0e6142131c
mateo <mateo.b1496@gmail.com>
2022-09-14 17:40:51 -0300

    cambios

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { useContext } from "react";
 import { FaShoppingCart } from "react-icons/fa"
+import { CartContext } from "./CartContext";
+import { Link } from "react-router-dom"
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 const CartWidget = () => {
+    const { totalCant } = useContext(CartContext)
     return(
-        <FaShoppingCart className="carrito"/>
+        <div>
+           <FaShoppingCart className="carrito"/>
+           <Link to="/Cart"><span className="badge bg-secondary m-lg-3 mb-9 carro">{totalCant()}</span></Link>
+        </div>
     );
 }
 
