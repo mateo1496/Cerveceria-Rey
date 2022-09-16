eb7a2e0277aa91512900b6e8238250291d6f6181
mateo <mateo.b1496@gmail.com>
2022-09-16 17:47:31 -0300

    cambios

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { products } from "../mock/products";
+import { collection, getDocs, query, where} from "firebase/firestore"
+import { db } from "../FirebaseConfig";
+// import { products } from "../mock/products";
 import ItemList from "../components/ItemList";
 import { FadeLoader } from 'react-spinners';
 
@@ -10,42 +12,26 @@ const ItemListContainer = (props) => {
   const [spinners, setSpinners] = useState(true);
 
   useEffect(()=>{
-    if(id){
-      const obtProducts = new Promise((res, rej)=>{
-        const prodFilter = products.filter((prod) => prod.category === id)
-        setTimeout(() =>{
-          res(prodFilter)
-        },2000);
-      });
-      
-      obtProducts
-      .then((correct)=>{
-        setItems(correct)
-        setSpinners(false)
+      const itemCollection = collection(db, "productos") //COLLECTION UTILIZA DOS PARAMETROS EL PRIMERO ENVIA LA BASE DE DATOS Y EL SEGUNDO EL NOMBRE DE TU COLECCION DE PRODUCTOS.
+      getDocs(!id ? itemCollection : query(itemCollection, where("category", "==", id))) //ES UNA PROMESA Y LE PASO COMO PARAMETRO LA REFERENCIA QUE TIENE LA BASE DE DATOS Y NUESTROS PRODUCTOS.
+      .then((resp)=>{
+        // console.log(resp.docs); PARA ACCEDER A LOS OBJETOS.
+        // console.log(prod.data()); //ASI INGRESO A LA INFORMACION DE CADA PRODUCTO.
+        const products = resp.docs.map((prod) =>{
+          return{ //PARA ACCEDER A LA INFORMACION DE LOS OBJETOS ES CON .DATA
+            id: prod.id, //AGREGAMOS AL ID COMO PROPIEDAD DEL ELEMENTO.
+            ...prod.data()
+          }
+        });
+        setItems(products);
+        setSpinners(false);
       })
-      .catch((error)=>{
-        //setItems(error)
+      .catch((error) =>{
+        console.log(error);
       });
-    } else {
-      const obtProducts = new Promise((res, rej)=>{
-        setTimeout(() =>{
-          res(products)
-        },2000);
-      });
-      
-      obtProducts
-      .then((correct)=>{
-        setItems(correct);
-        setSpinners(false)
-      })
-      .catch((error)=>{
-        //setItems(error)
-      });
-
       return () =>{
-        setSpinners(true);
+            setSpinners(true);
       }
-    }
   }, [id]); //CON LA VARIABLE ID EN EL ARRAY DE DEPENDENCIA, CADA CATEGORIA MUESTRA SUS PRODUCTOS.
 
   // console.log(items)
@@ -55,7 +41,7 @@ const ItemListContainer = (props) => {
     <div>
       {
         spinners
-        ? <FadeLoader  color="rgba(240, 113, 9, 0.9)" margin="10px"  size={150} /> 
+        ? <FadeLoader  color="rgba(240, 113, 9, 0.9)" margin="10px"   size={150} /> 
         : <>
            <h1 className='titleTop'>{props.saludo}</h1>
            <ItemList items={items} />
@@ -67,3 +53,39 @@ const ItemListContainer = (props) => {
 
 export default ItemListContainer;
 
+// if(id){
+//   const obtProducts = new Promise((res, rej)=>{
+//     const prodFilter = products.filter((prod) => prod.category === id)
+//     setTimeout(() =>{
+//       res(prodFilter)
+//     },2000);
+//   });
+  
+//   obtProducts
+//   .then((correct)=>{
+//     setItems(correct)
+//     setSpinners(false)
+//   })
+//   .catch((error)=>{
+//     //setItems(error)
+//   });
+// } else {
+//   const obtProducts = new Promise((res, rej)=>{
+//     setTimeout(() =>{
+//       res(products)
+//     },2000);
+//   });
+  
+//   obtProducts
+//   .then((correct)=>{
+//     setItems(correct);
+//     setSpinners(false)
+//   })
+//   .catch((error)=>{
+//     //setItems(error)
+//   });
+
+//   return () =>{
+//     setSpinners(true);
+//   }
+// }
