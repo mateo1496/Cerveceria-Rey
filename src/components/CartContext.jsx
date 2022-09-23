80a9fbe546f92fd9a63c3195b42701abd5ac88e6
mateo <mateo.b1496@gmail.com>
2022-09-23 11:59:03 -0300

    componente logica

--- src/components/CartContext.jsx
+++ src/components/CartContext.jsx
@@ -5,56 +5,49 @@ export const CartContext = createContext();
 
 const CartProvider = ({ children }) =>{
     const [cart, setCart] = useState ([]);
-    //FUNCIONES
-    //FUNCION PARA AGREGAR UNA CANTIDAD DEL ITEM AL CARRITO
+   
     const addItem = (item, q) =>{
-        // console.log({...item, q}); //AGREGO AL ITEM LA PROPIEDAD CANTIDAD.
-        if (isInCart(item.id)) { //CON LA FUNCION ACA, ACCEDO AL ITEM. (ACCEDO A LA PROPIEDAD ID DEL ITEM, SE LA ENVIO A LA FUNCION ISINCART)
-            sumarCantidad(item, q);                                  //SI EL PRODUCTO ESTA LO AGREGO AL CARRITO.
+        if (isInCart(item.id)) { 
+            sumarCantidad(item, q);                                  
         } else {
-            setCart([...cart,{...item, q}]); //DE ESTA FORMA AL PRODUCTO QUE AGREGUE AL CARRITO, AGREGA UN NUEVO PRODUCTO QUE COMPRO. SINO SETEO EL CARRITO.
+            setCart([...cart,{...item, q}]); 
         }
     }
 
     const sumarCantidad = (item, q) =>{
         const newList = cart.map((prod) =>{
-            if (prod.id === item.id) { //PREGUNTO QUE PRODUCTO TENGO QUE ACTUALIZAR. PREGUNTO SI EL PROD.ID ES IGUAL AL ID QUE ESTOY AGREGANDO.
-                const prodNew = { //PARA AGREGAR EL PRODUCTO SI TODAVIA NO LO AGREGUE AL CARRITO (CART)
-                    ...prod, //CON SPREAD HAGO LA ITERACION DEL PRODUCTO, AGREGO EL PRODUCTO Y SUS PROPIEDADES + CANTIDAD. SOLO ACTUALIZO LA CANTIDAD
-                    q:  q //NO SE ACUMULA CANTIDAD Y NO SE PASA DEL STOCK.
+            if (prod.id === item.id) { 
+                const prodNew = { 
+                    ...prod, 
+                    q:  q 
                 }
-                return prodNew //SI EL PRODUCTO YA ESTA, LO DEJO
+                return prodNew 
             } else {
                 return prod
             }
         })
-        setCart(newList); //ACTUALIZAR EL ESTADO
+        setCart(newList); 
     } 
 
-    // FUNCION ISINCART PARA CORROBAR QUE EL PRODUCTO YA ESTA EN EL CARRITO
-    const isInCart = (id) => { //ESTA FUNCION RECIBE AL ID POR ARGUMENTO Y LO UTILIZA PARA COMPARAR, RECCORE EL ARRAY Y PREGUNTA SI HAY UN ID, Y DEVUELVE TRUE O FALSE.
-        return cart.some((prod) => prod.id === id); //SOME METODO QUE DEVUELVE TRUE O FALSE, SI EL PRODUCTO YA SE LO AGREGO AL CARRITO O NO.
+    const isInCart = (id) => { 
+        return cart.some((prod) => prod.id === id); 
     }
 
-    // FUNCION PARA REMOVER UN ITEM DEL CART USANDO SU ID
-    const removeItem = (id) =>{ //LA FUNCION DEL CONTEXTO ME LLEGA COMO ARGUMENTO QUE SERIA EL ID.
+    const removeItem = (id) =>{ 
         console.log(`Eliminando prodcuto ${id}`);
-        const deleteProd = cart.filter((prod) => prod.id !== id); //ELIMINAR UN PRODUCTO
+        const deleteProd = cart.filter((prod) => prod.id !== id); 
         setCart(deleteProd);
     }
 
-    // FUNCION PARA REMOVER TODOS LOS ITEMS
     const clear = () =>{
         setCart([]);
     }
 
-    //PARA SABER LA CANTIDAD DE UN PRODUCTO
     const prodCant = (id) =>{
-        const cantidad = cart.find((prod)=> prod.id === id); //RECORRO EL CARRITO Y VEO SI EL ID YA ESTA EN EL CARRITO
-        return cantidad?.q //RETORNAR LA CANTIDAD DEL PRODUCTO, SI EL PRODUCTO NO ESTA EN EL CARRITO RETORNA UNDEFINED. CON EL ? (OPTIONAL CHAINING) PERMITE QUE SI EL PRODUCTO NO ESTA EN EL CARRITO NO ARROJE UNDEFINED, SINO ES UNDEFINED RETORNA LA CANTIDAD.
+        const cantidad = cart.find((prod)=> prod.id === id);
+        return cantidad?.q 
     }
 
-    //PARA CALCULAR EL TOTAL
     const totalPrice = () =>{
         let totalProd = 0
         cart.forEach((prod) =>{
@@ -63,7 +56,6 @@ const CartProvider = ({ children }) =>{
         return totalProd;
     }
 
-    //PARA CALCULAR EL TOTAL DE UNIDAD DEL CARRITO
     const totalCant = () =>{
         let totalCantProd = 0
         cart.forEach((prod)=>{
